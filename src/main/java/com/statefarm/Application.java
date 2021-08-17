4d3f0c1c29bcf727dc32bc5db0eb19d5a3915131
Sequita Bridges <sequita.bridges.luol@statefarm.com>
2021-08-17 08:45:08 -0700

    added logic to program and it is working

--- src/main/java/com/statefarm/Application.java
+++ src/main/java/com/statefarm/Application.java
@@ -4,6 +4,17 @@ public class Application {
 
     public static void main(String[] args) {
 
+        int x = 0;
+
+        for(int j = 0; j < args.length; j++) {
+            int num = Integer.parseInt(args[j]);
+            //System.out.println(args[j]);
+
+            if(num > 0){
+                x += num;
+            }
+        }
+        System.out.println(x);
     }
 
 }
